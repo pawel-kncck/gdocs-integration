280783d43d1c00b77b2c675c9051a42695ad1a8d
Pawel Kuncicki <pawel.kuncicki@gmail.com>
2020-09-30 16:34:33 +0200

    authorization and getting the json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import config from './config';
+import Button from '@material-ui/core/Button';
+import { TextField } from '@material-ui/core';
+// import { gapi } from 'gapi';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 'auto',
+    maxWidth: '650px'
+  },
+  h1: {
+    marginTop: '30px',
+    marginBottom: '50px',
+  },
+  buttonContainer: {
+    display: 'flex',
+    margin: '30px 0'
+  }
+}
 
 function App() {
+  const [json, setJson] = useState('')
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [docId,setDocId] = useState('');
+  const gapi = window.gapi;
+
+  const initClient = () => {
+    gapi.client.init({
+      apiKey: config.apiKey,
+      clientId: config.cliendId,
+      discoveryDocs: config.discoveryDocs,
+      scope: config.scope
+    }).then(() => {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(setIsSignedIn(isSignedIn));
+      // Handle the initial sign-in state
+      setIsSignedIn(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
+  }
+
+  useEffect(() => {
+      gapi.load('client:auth2', initClient);
+  },[])
+
+  const handleAuthClick = () => {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+
+  const handleSignoutClick = () => {
+    gapi.auth2.getAuthInstance().signOut();
+  }
+
+  const handleGetJson = () => {
+    window.gapi.client.docs.documents.get({
+      documentId: '1xVxwds3cdBAHPxYfIuIJxsYqCpi9hAOU2-qcqdoFoIQ'
+    })
+    .then(response => {
+      const doc = response.result;
+      const body = doc.body;
+      setJson(body);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  const parseDocumentUrl = (url) => {
+    setDocId(url.match(/[-\w]{25,}/));
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.root}>
+      <h1 style={styles.h1}>Google Docs Integration</h1>
+      <TextField variant='standard' placeholder='Document ID' onChange={(event) => parseDocumentUrl(event.target.value)} />
+      {docId}
+      <div style={styles.buttonContainer}>
+        {isSignedIn
+          ? <Button onClick={handleSignoutClick} variant='outlined' color='primary'>Sign out</Button>
+          : <Button onClick={handleAuthClick} variant='outlined' color='primary'>Authorize</Button>
+        }
+      </div>
+      <div>
+        <button onClick={handleGetJson}>GET</button>
+      </div>
+      <pre>
+        {json ? JSON.stringify(json, null, 4) : null}
+      </pre>
     </div>
   );
 }
